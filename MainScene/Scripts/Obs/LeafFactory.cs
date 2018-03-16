da7fc191e2ec9c748dc35cfc0f608cde6482d802
GrapesOne <m.d.f@ro.ru>
2018-03-16 23:27:09 +0500

    LeafFactory0.1
    Падающие листья первая версия

--- MainScene/Scripts/Obs/LeafFactory.cs
+++ MainScene/Scripts/Obs/LeafFactory.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class LeafFactory : MonoBehaviour {
 
-	[SerializeField] GameObject Leaf = new GameObject();
+	public GameObject Leaf;
+	GameObject NewLeaf;
 	float xRand, timer = 0f;
+
+	CameraManage CamManager;
+	Camera Cam;
 	
 	void Start(){
 		timer += Random.Range (0.1f, 2f);
+
+		CamManager = GameObject.FindGameObjectWithTag ("Camera").GetComponent<CameraManage> ();
+		Cam = GameObject.FindGameObjectWithTag ("Camera").GetComponent<Camera> ();
 	}
 
 	void Update () {
@@ -16,11 +23,19 @@ public class LeafFactory : MonoBehaviour {
 			timer -= Time.deltaTime;
 		 else {
 			xRand = Random.Range (-1f, 1);
-			Instantiate (Leaf, new Vector3 (transform.position.x + xRand, transform.position.y, transform.position.z), 
-				Quaternion.identity);
+			NewLeaf = Instantiate (Leaf, new Vector3 (transform.position.x + xRand, transform.position.y, transform.position.z), 
+				Quaternion.identity) as GameObject;
+			NewLeaf.transform.SetParent (transform.parent);
 			timer = 0f + Random.Range (0.1f, 2f);
 		}
+	}
 
 
+	void FixedUpdate () {
+		if (Mathf.Abs (transform.position.x - CamManager.ThisCamTransform.position.x) >
+			Cam.orthographicSize * 2 + 5 || 
+			Mathf.Abs (transform.position.y - CamManager.ThisCamTransform.position.y) >
+			Cam.orthographicSize * 2 + 5)
+			timer = 0f + Random.Range (0.1f, 2f);
 	}
 }
---
+++ MainScene/Scripts/Obs/UnactOutsideCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnactOutsideCamera : MonoBehaviour {
+
+	CameraManage CamManager;
+	Camera Cam;
+	void Start () {
+		CamManager = GameObject.FindGameObjectWithTag ("Camera").GetComponent<CameraManage> ();
+		Cam = GameObject.FindGameObjectWithTag ("Camera").GetComponent<Camera> ();
+	}
+
+
+	void FixedUpdate () {
+		if (Mathf.Abs (transform.position.x - CamManager.ThisCamTransform.position.x) >
+			Cam.orthographicSize * 2 + 7 || 
+			Mathf.Abs (transform.position.y - CamManager.ThisCamTransform.position.y) >
+			Cam.orthographicSize * 2 + 7)
+			gameObject.transform.GetChild (0).gameObject.SetActive (false);
+		else
+			gameObject.transform.GetChild (0).gameObject.SetActive (true);
+	}
+}
+
